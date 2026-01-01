b6ac3423f6237116d2be90dbb029202538f75607
prathamm2506 <fs20if005@gmail.com>
2026-01-01 10:51:30 +0530

    Version 1.0

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,22 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import logo from '../../assets/logo_astronaut_no_bg.png'
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "../../assets/logo_astronaut_no_bg.png";
 
 const navigation = [
-  { name: 'About', href: '#about' },
-  { name: 'Features', href: '#features' },
-  { name: 'Global', href: '#global' },
-  { name: 'Connect', href: '#mail' },
-]
+  { name: "About", href: "#about" },
+  { name: "Features", href: "#features" },
+  { name: "Global", href: "#global" },
+  { name: "Connect", href: "#mail" },
+];
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-[var(--grey)]" >
+    <div className="bg-[var(--grey)]">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           aria-label="Global"
@@ -26,11 +26,7 @@ export default function Header() {
           <div className="flex lg:flex-1">
             <a href="" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src={logo}
-                className="h-10 w-auto"
-              />
+              <img alt="" src={logo} className="h-10 w-auto" />
             </a>
           </div>
 
@@ -62,7 +58,7 @@ export default function Header() {
           {/* Desktop login */}
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
-              target='_blank'
+              target="_blank"
               href="https://www.linkedin.com/in/the-orbitalist-048b263a1/"
               className="text-sm font-medium text-[var(--white)] hover:text-[var(--less-white)] transition"
             >
@@ -72,13 +68,17 @@ export default function Header() {
         </nav>
 
         {/* Mobile menu */}
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[var(--grey)] p-6 sm:max-w-sm sm:ring-1 sm:ring-[var(--white)]/10">
             <div className="flex items-center justify-between">
               <img
                 alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
+                src={logo}
                 className="h-8 w-auto"
               />
               <button
@@ -97,6 +97,7 @@ export default function Header() {
                     <a
                       key={item.name}
                       href={item.href}
+                      onClick={() => setMobileMenuOpen(false)}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium text-[var(--white)] hover:bg-white/5 transition"
                     >
                       {item.name}
@@ -106,8 +107,9 @@ export default function Header() {
 
                 <div className="py-6">
                   <a
-                    target='_blank'
+                    target="_blank"
                     href="https://www.linkedin.com/in/the-orbitalist-048b263a1/"
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-medium text-[var(--white)] hover:bg-white/5 transition"
                   >
                     LinkedIn
@@ -122,7 +124,8 @@ export default function Header() {
       {/* Hero section */}
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 text-center">
-         <span className="
+          <span
+            className="
   inline-flex items-center
   whitespace-nowrap
   text-[color:var(--less-white)]
@@ -130,20 +133,21 @@ export default function Header() {
   px-3 py-1
   rounded-full
   text-xs sm:text-sm
-">
-  Earth & Space Intelligence for Climate,
-  <span className="text-[color:var(--red)] ml-1 font-medium">
-    CSR & ESG Decisions
-  </span>
-</span>
-
+"
+          >
+            Earth & Space Intelligence for Climate,
+            <span className="text-[color:var(--red)] ml-1 font-medium">
+              CSR & ESG Decisions
+            </span>
+          </span>
 
           <h1 className="text-4xl font-semibold tracking-tight text-[var(--white)] sm:text-5xl lg:text-6xl mt-5">
             Turning Earth & Space Data into Intelligence
           </h1>
 
           <p className="mt-6 text-base text-[var(--less-white)] sm:text-lg">
-            Built for credibility. Designed for impact. Powered by data from space.
+            Built for credibility. Designed for impact. Powered by data from
+            space.
           </p>
 
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -164,5 +168,5 @@ export default function Header() {
         </div>
       </div>
     </div>
-  )
+  );
 }
